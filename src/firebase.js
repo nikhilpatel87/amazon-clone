024492aa6bb47082a09cf70fb62b11a129f423da
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-03 16:02:40 -0600

    1. Initial Setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBKQcBcRw5mOTScH6y94IgkQxCJij7hQIM',
+	authDomain: 'amzn-clone-np.firebaseapp.com',
+	projectId: 'amzn-clone-np',
+	storageBucket: 'amzn-clone-np.appspot.com',
+	messagingSenderId: '871755045091',
+	appId: '1:871755045091:web:98b74434d54827053ecdd6',
+	measurementId: 'G-2Q8WDR20PZ',
+};
