9547ec4d86fd64b3d61caf8b228ba22619a54e1e
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-03 22:34:36 -0600

    8. initial checkout page setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
 
 function App() {
 	return (
@@ -10,6 +11,12 @@ function App() {
 		<div className='app'>
 			<Router>
 				<Switch>
+					<Route path='/checkout'>
+						<Header />
+						<Checkout />
+					</Route>
+
+					{/* keep default Route at the bottom!!!! */}
 					<Route path='/'>
 						<Header />
 						<Home />
