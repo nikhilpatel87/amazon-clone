da720b50ad727fd7954b505f3bf165219ffc983d
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-03 18:34:27 -0600

    3. Nav Bar

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<div className='home'>
+			<div className='home__container'>
+				<img
+					className='home__image'
+					src='https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/Other/AWRD_21_02498_GWBleedingHero_1500x600_Final_en-US_PVD6553_Generic._CB661165540_.jpg'
+					alt='Banner'
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
