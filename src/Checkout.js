b53eba68ba9935b8a7985959bce0b5bc151057c6
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-05 14:34:52 -0600

    10. Subtotal Styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
 	return (
@@ -22,7 +23,6 @@ function Checkout() {
 			</div>
 			<div className='checkout__right'>
 				<Subtotal />
-				<h2>The Subtotal will go here</h2>
 			</div>
 		</div>
 	);
