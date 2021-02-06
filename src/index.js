b5ea7ef8577323e745626fdb4f25aea545ab27f5
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-05 18:22:00 -0600

    11. Add reducer and StateProvider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import reducer, { initialState } from './reducer';
-import { stateProvider } from './StateProvider';
+import { StateProvider } from './StateProvider';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<StateProvider initialState={initialState} reducer={reducer}>
+			<App />
+		</StateProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
