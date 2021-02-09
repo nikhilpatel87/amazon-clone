ae90398682794f7cc1ff2199494c6df7bb94cfdb
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-09 10:46:05 -0600

    17. Login Styling

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firebase';
 
 function Login() {
+	const history = useHistory();
+	const [email, setEmail] = useState('email');
+	const [password, setPassword] = useState('');
+
+	const signIn = (e) => {
+		e.preventDefault();
+
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then((auth) => {
+				history.push('/');
+			})
+			.catch((error) => alert(error.message));
+	};
+
+	const register = (e) => {
+		e.preventDefault();
+
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((auth) => {
+				if (auth) {
+					history.push('/');
+				}
+			})
+			.catch((error) => alert(error.message));
+	};
 	return (
 		<div className='login'>
 			<Link to='/'>
@@ -16,9 +44,25 @@ function Login() {
 				<h1>Sign-In</h1>
 				<form>
 					<h5>Email</h5>
-					<input type='text' />
+					<input
+						type='text'
+						value={email}
+						onChanage={(e) => setEmail(e.target.value)}
+					/>
+
 					<h5>Password</h5>
-					<input type='password' />
+					<input
+						type='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<button
+						type='submit'
+						onClick={signIn}
+						className='login__signInButton'>
+						Sign In
+					</button>
 				</form>
 
 				<p>
@@ -26,6 +70,10 @@ function Login() {
 					Please see our Privacy Notice, our Cookies Notice and our
 					Interest-Based Ads Notice. This is not just a cloned site of Amazon.
 				</p>
+
+				<button onClick={register} className='login__registerButton'>
+					Create Your Amazon Account
+				</button>
 			</div>
 		</div>
 	);
